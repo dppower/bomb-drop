2407675fbc8186dfa07015a8b3e0b09d003de7d1
dppower <powerdp@gmail.com>
2018-02-19 11:48:09 +0000

    Worked on score and timer layout.

--- app/canvas/score-display.component.ts
+++ app/canvas/score-display.component.ts
@@ -1,4 +1,4 @@
-﻿import { Component, OnInit, OnDestroy } from "@angular/core";
+﻿import { Component, OnInit, OnDestroy, HostBinding } from "@angular/core";
 
 import { Subscription } from "rxjs/Subscription";
 import { scan, map, bufferTime } from "rxjs/operators";
@@ -8,24 +8,67 @@ import { ScoreTracker } from "../game/score-tracker";
 @Component({
     selector: "score-display",
     template: `
-    <p>Score:</p>
+    <p>Score</p>
     <p>{{score_total}}</p>
     <p>{{score_change}}</p>
     `,
     styles: [`
     :host {
-        position: absolute; z-index: 1;
+        position: absolute;
+        z-index: 1;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+    }
+    p {
+        font-size: 32px;
+        text-align: center;
+        height: 48px;
+        line-height: 48px;
+        width: 100px;
     }
     `]
 })
 export class ScoreDisplay implements OnInit, OnDestroy {
+
+    @HostBinding("style.width")
+    get style_width() {
+        if (this.input_manager_.aspect <= this.input_manager_.world_aspect) {
+            return "100%";
+        }
+        else {
+            return "auto";
+        }
+    };
+
+    @HostBinding("style.flex-direction")
+    get style_flex_direction() {
+        if (this.input_manager_.aspect <= this.input_manager_.world_aspect) {
+            return "row";
+        }
+        else {
+            return "column";
+        }
+    };
+
+    @HostBinding("style.justify-content")
+    get style_justify_content() {
+        if (this.input_manager_.aspect <= this.input_manager_.world_aspect) {           
+            return "space-around";
+        }
+        else {
+            return "flex-start";
+        }
+    };
 
     score_total: number = 0;
     score_change: number;
 
     private score_change_sub_: Subscription;
 
-    constructor(private score_tracker_: ScoreTracker, private input_manager_: InputManager) { };
+    constructor(private score_tracker_: ScoreTracker,
+        private input_manager_: InputManager
+    ) { };
 
     ngOnInit() {
         this.score_change_sub_ = this.score_tracker_.scores
--- app/canvas/swap-timer.component.ts
+++ app/canvas/swap-timer.component.ts
@@ -1,21 +1,82 @@
-﻿import { Component, OnInit, OnDestroy } from "@angular/core";
+﻿import { Component, OnInit, OnDestroy, HostBinding } from "@angular/core";
+
 import { RenderLoop } from "./render-loop";
+import { InputManager} from "./input-manager";
 
 import { Subscription } from "rxjs/Subscription";
 import { map, distinctUntilChanged } from "rxjs/operators";
 
 @Component({
     selector: "swap-timer",
-    template: "<p>Time to next swap: {{time_next_swap}}</p>",
-    styles: [`p {position: absolute; z-index: 1;}`]
+    template: `
+    <p id="time">{{time_next_swap}}</p>
+    <p id="time-label">Time to next swap</p>
+    `,
+    styles: [`
+    :host {
+        position: absolute;
+        z-index: 1;
+        display: flex;
+        flex-direction: column;
+        right: 0;
+        bottom: 0;
+        height: 100%;
+        justify-content: flex-end;
+        align-items: center;
+    }
+    p#time {
+        font-size: 52px;
+        width: 78px;
+        height: 78px;
+        text-align: center;
+        line-height: 78px;
+    }
+    p#time-label{
+        font-size: 20px;
+        width: 180px;
+        height: 78px;
+        text-align: center;
+        line-height: 78px;
+    }
+    `]
 })
 export class SwapTimer implements OnInit, OnDestroy {
+
+    @HostBinding("style.width")
+    get style_width() {
+        if (this.input_manager_.aspect <= this.input_manager_.world_aspect) {
+            return "100%";
+        }
+        else {
+            return "auto";
+        }
+    };
+
+    @HostBinding("style.height")
+    get style_height() {
+        if (this.input_manager_.aspect <= this.input_manager_.world_aspect) {           
+            return "auto";
+        }
+        else {
+            return "100%";
+        }
+    };
+
+    @HostBinding("style.flex-direction")
+    get style_flex_direction() {
+        if (this.input_manager_.aspect <= this.input_manager_.world_aspect) {
+            return "row";
+        }
+        else {
+            return "column";
+        }
+    };
 
     time_next_swap: number;
 
     time_updates_sub: Subscription;
 
-    constructor(private render_loop_: RenderLoop) { };
+    constructor(private render_loop_: RenderLoop, private input_manager_: InputManager) { };
 
     ngOnInit() {
         this.time_updates_sub = this.render_loop_.swap_interval
